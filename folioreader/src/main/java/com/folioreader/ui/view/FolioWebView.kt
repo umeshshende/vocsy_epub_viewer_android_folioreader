a9dc124c3212da8242ae4fb1658b6d393444ff2f
umesh <umesh.shende19@gmail.com>
2024-03-02 23:16:52 +0530

    Restrict Copy And Share

--- folioreader/src/main/java/com/folioreader/ui/view/FolioWebView.kt
+++ folioreader/src/main/java/com/folioreader/ui/view/FolioWebView.kt
@@ -350,13 +350,15 @@ class FolioWebView : WebView {
         when (id) {
             R.id.copySelection -> {
                 Log.v(LOG_TAG, "-> onTextSelectionItemClicked -> copySelection -> $selectedText")
-                UiUtil.copyToClipboard(context, selectedText)
-                Toast.makeText(context, context.getString(R.string.copied), Toast.LENGTH_SHORT)
+               // UiUtil.copyToClipboard(context, selectedText)
+                Toast.makeText(context, context.getString(R.string.DONOTALLOWED), Toast.LENGTH_SHORT)
                     .show()
             }
             R.id.shareSelection -> {
                 Log.v(LOG_TAG, "-> onTextSelectionItemClicked -> shareSelection -> $selectedText")
-                UiUtil.share(context, selectedText)
+                //UiUtil.share(context, selectedText)
+                Toast.makeText(context, context.getString(R.string.DONOTALLOWED), Toast.LENGTH_SHORT)
+                    .show()
             }
             R.id.defineSelection -> {
                 Log.v(LOG_TAG, "-> onTextSelectionItemClicked -> defineSelection -> $selectedText")
